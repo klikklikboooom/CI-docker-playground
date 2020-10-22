b99d75e02f0ad636be607d1bd889eefa79b75253
Nikhil Anantharaman <nikhilanantharaman@gmail.com>
2020-10-22 13:33:49 +0530

    reverted change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch
+          Learn React
         </a>
       </header>
     </div>
